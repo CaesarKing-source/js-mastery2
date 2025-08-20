bc704c3e7f90c3a437b8ba544406bd61d70e24cd
CaesarKing-source <coder2880@gmail.com>
2025-08-20 12:40:40 +0530

    third commit

--- async-await/main.js
+++ async-await/main.js
@@ -1,26 +1,13 @@
-// Factory Function
-let chatWindow = document.querySelector('.chatWindow');
-function UserDetails(name, email, color, type) {
-    this.name = name;
-    this.email = email;
-    this.color = color;
-    this.type = type; 
-}
+const pr = new Promise(function (res, rej) {
+  setTimeout(() => {
+    let num = Math.floor(Math.random() * 10);
+    if (num > 5) res(num);
+    else rej(num);
+  }, 2000);
+});
 
-UserDetails.prototype.showInfo = function() {
-    console.log(`Name: ${this.name}, Email: ${this.email}`);
-}
-
-UserDetails.prototype.write = function(message) {
-    const chat = document.createElement('p');
-    if(this.type == 'sender') {
-        chat.className = `text-start bg-gray-200 w-[max-content] px-2 text-black rounded`;
-    }
-    else chat.className = `text-end bg-green-200 w-[max-content] px-2 text-black rounded`;
-    chat.textContent = message;
-    chatWindow.append(chat);
-}
-
-const user1 = new UserDetails('Akshay G', 'test@mail.com', 'gray', 'receiver');
-const user2 = new UserDetails('Test User 02', 'test2@mail.com', 'green', 'sender');
-
+pr.then(function (num) {
+  console.log("Resolve With", num);
+}).catch(function (num) {
+  console.log("Rejected with", num);
+});
--- async-await/main2.js
+++ async-await/main2.js
@@ -1,41 +1,18 @@
-const chatWindow = document.querySelector('.chatWindow');
-class UserDetails {
-    constructor(name, email, color, type) {
-        this.name = name;
-        this.email = email;
-        this.color = color;
-        this.type = type;
-        this.role = 'user';
-    }
+const pr = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        let num = Math.floor(Math.random() * 10);
+        if(num > 5) resolve(num);
+        else reject(num);
+    }, 4000);
+});
 
-    showInfo() {
-        console.log(`Name: ${this.name} || Email: ${this.email} || Role: ${this.role}`);
+async function testNum() {
+    try {
+        let value = await pr;
+        console.log('Resolved with', value)
     }
-
-    write(message) {
-        const chat = document.createElement('p');
-        if(this.type == 'sender') {
-            chat.className = `text-start bg-gray-200 w-[max-content] px-2 text-black rounded`;
-        }
-        else {
-            chat.className = `text-end bg-green-200 w-[max-content] px-2 text-black rounded`;
-        }
-        chat.textContent = message;
-        chatWindow.append(chat);
+    catch(err) {
+        console.log('Rejected with', err);
     }
 }
-
-class AdminDetails extends UserDetails {
-    constructor(name, email, color, type) {
-        super(name, email, color, type)
-        this.role = 'admin'
-    }
-
-    removeChats() {
-        chatWindow.innerHTML = '';
-    }
-}
-let user1 = new UserDetails('Test User 01', 'test@mail.com', 'gray', 'sender');
-let user2 = new UserDetails('Test User 02', 'test2@mail.com', 'green', 'receiver');
-let admin1 = new AdminDetails('Akshay Giri', 'admin@mail.com', 'blue', 'sender');
-
+testNum()
